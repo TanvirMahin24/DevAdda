badd624c3629fac3f8765b3ec620747355ff581d
Tanvir Mahin <55342128+TanvirMahin24@users.noreply.github.com>
2020-06-07 22:03:46 +0600

    production

--- server.js
+++ server.js
@@ -1,23 +1,32 @@
-const express = require('express');
-const connectDB = require('./config/db');
+const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 //Initializing the middlewares
-app.use(express.json({ extended : false}));
-
+app.use(express.json({ extended: false }));
 
 //Define route
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/posts', require('./routes/api/posts'));
-app.use('/api/profile', require('./routes/api/profile'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
+
+//serve static asset
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 //Connect to MongoDB
 connectDB();
 
 //Express Server started
 app.listen(PORT, () => {
-    console.log(`Server running at port: ${PORT}`);
-})
+  console.log(`Server running at port: ${PORT}`);
+});
