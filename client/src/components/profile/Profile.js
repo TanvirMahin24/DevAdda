2be9ae92f8ec9dd0a56fbdc541208f53d909c5f7
Tanvir Mahin <55342128+TanvirMahin24@users.noreply.github.com>
2020-06-10 11:06:49 +0600

    Update Profile.js

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -51,7 +51,7 @@ const Profile = ({
               {auth.isAuthenticated &&
                 auth.loading === false &&
                 auth.user._id === profile.user._id && (
-                  <Link to="/edit-profile" className="btn btn-dark">
+                  <Link to="/edit-profile" className="btn btn-primary mt-3 mb-4">
                     Edit Profile
                   </Link>
                 )}
