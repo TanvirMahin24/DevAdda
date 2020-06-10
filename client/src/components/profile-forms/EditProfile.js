75d21dfce756469023319864f6898bf775ae9a2c
Tanvir Mahin <55342128+TanvirMahin24@users.noreply.github.com>
2020-06-10 11:24:11 +0600

    Update EditProfile.js

--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -72,7 +72,7 @@ const EditProfile = ({
   };
   return (
     <div className="container card card-body shadow shadow-sm my-3">
-      <h1 className="large text-primary">Create Your Profile</h1>
+      <h1 className="text-dark">Create Your Profile</h1>
       <p className="lead">
         <i className="fas fa-user"></i> Let's get some information to make your
         profile stand out
